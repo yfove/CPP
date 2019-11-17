2235a362fa8859dba9d6ca70fa3503a8418948d6
curtis chat <curtis.ch.chat@gmail.com>
2019-11-17 13:34:33 -0700

    new exercises

---
+++ vectors/size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+    
+  // Add more 
+  grocery.push_back("Artichoke");
+  
+  std::cout << grocery.size() << "\n";
+
+
+}
